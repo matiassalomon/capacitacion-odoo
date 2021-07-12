a320b527f64503afef239c8089c8df47aa72d274
Matias Salomon <odoo@ampd1.odoo.com>
2021-07-12 17:26:30 +0000

    Creacion del primer modulo

---
+++ primer_modulo/__manyfest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Mi primer módulo',
+    'summary': """Ejemplo de un módulo para Odoo""",
+    'description': """
+        Ejemplo de un módulo para Odoo en Phyton que permitirá:
+            - Probar si puedo agregar módulos a Odoo.
+            - Extender funcionalidad sobre módulos existentes.
+            - Probar mis conocimientos.
+    """,
+    'author': 'Matías Salomón',
+    'website': 'https://kdoce.cl',
+    'category': 'Training',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [
+        
+    ],
+    'demo': [
+        
+    ]
+}
