8457191fe66c167485d2949c79f90ed416e35cd5
Matias Salomon <msalomon@kdoce.cl>
2021-07-13 13:03:30 +0000

    Inicio modulo Odoo Academy

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Odoo Academy',
+    'summary': """Ejemplo de un módulo para Odoo""",
+    'description': """
+        Ejemplo de un módulo para Odoo en Phyton que permitirá:
+            - Probar si puedo agregar módulos a Odoo.
+            - Extender funcionalidad sobre módulos existentes.
+            - Probar mis conocimientos.
+    """,
+    'author': 'Matías Salomón',
+    'website': 'https://kdoce.cl',
+    'category': 'Training',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [
+        
+    ],
+    'demo': [
+        
+    ]
+}
