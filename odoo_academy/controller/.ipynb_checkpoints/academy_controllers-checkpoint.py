3e1256fdea79d153f5d80194e791a263a2c3bafb
Matias Salomon <msalomon@kdoce.cl>
2021-07-22 20:45:58 +0000

    Otro más

--- odoo_academy/controller/.ipynb_checkpoints/academy_controllers-checkpoint.py
+++ odoo_academy/controller/.ipynb_checkpoints/academy_controllers-checkpoint.py
@@ -10,12 +10,12 @@ class Academy(http.Controller):
     @http.route('/academy/courses/', auth='public', website=True)
     def courses(self, **kw):
         courses = http.request.env['academy.course'].search([])
-        return http.request.render('odoo_academy.course.wesite', {
+        return http.request.render('odoo_academy.course.website', {
             'courses': courses,
         })
     
     @http.route('/academy/<model("academy.session"):session>/', auth='public', website=True)
     def session(self, session):
-        return http.request.render('odoo_academy.session.wesite', {
+        return http.request.render('odoo_academy.session.website', {
             'session': session,
         })
